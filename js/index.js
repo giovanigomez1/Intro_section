93e2a19ffae3c9ee1055eb8e2f8e833c38cee2ce
giovanigomez1 <giovani-gomez1@hotmail.com>
2024-03-02 06:47:36 -0600

    Mobile version completed

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector('.nav')
+const nav__menu = nav.querySelector('.nav__menu')
+const menu_open = nav.querySelector('.nav__menuicon-open')
+const menu_close = nav.querySelector('.nav__menuicon-close')
+const overlay = document.querySelector('.overlay')
+
+
+const toggleClass = (menu) => {
+  menu_open.classList.toggle('hidden')
+  menu_close.classList.toggle('hidden')
+  overlay.classList.toggle('hidden')
+  nav__menu.style.transform = `translateX(${menu === 'open' ? '-' : ''}100%)`
+}
+
+nav.addEventListener('click', function(e) {
+  if(e.target.classList.contains('nav__menuicon-open')) {
+    toggleClass('open')
+  }
+  if(e.target.classList.contains('nav__menuicon-close')) {
+    toggleClass('close')
+  }
+})
+
